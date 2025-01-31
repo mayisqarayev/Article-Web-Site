4bdf50df541fbc1ef01d47740db734521ac7a7ef
mayis <mayisqarayev75@gmail,com>
2025-01-31 22:21:37 +0400

    redis

---
+++ Article-Web-Site/src/main/java/com/Article/Web/Site/config/CacheConfig.java
@@ -0,0 +1,25 @@
+package com.Article.Web.Site.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager.builder(redisConnectionFactory).build();
+    }
+
+}
--- Article-Web-Site/src/main/java/com/Article/Web/Site/service/ImageService.java
+++ Article-Web-Site/src/main/java/com/Article/Web/Site/service/ImageService.java
@@ -2,9 +2,14 @@ package com.Article.Web.Site.service;
 
 import com.Article.Web.Site.converter.ImageConverter;
 import com.Article.Web.Site.dto.request.AddImageRequestDto;
+import com.Article.Web.Site.dto.response.ImageResponseDto;
 import com.Article.Web.Site.model.ImageEntity;
 import com.Article.Web.Site.repo.ImageRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ImageService {
@@ -24,6 +29,14 @@ public class ImageService {
                 .imageStatus(true)
                 .fkArticleId(requestDto.getFkArticleId()).build()
         );
+    }
+
+    @Cacheable(value = "images", key = "#articleId")
+    public List<ImageResponseDto> getImagesByArticleId(String articleId) {
+        return repository.findAll().stream()
+                .filter(imageEntity -> imageEntity.getFkArticleId().equals(articleId))
+                .map(imageEntity -> new ImageResponseDto(imageEntity.getImageUrl()))
+                .collect(Collectors.toList());
     }
 
     protected ImageEntity transformImageEntity(AddImageRequestDto requestDto) {
